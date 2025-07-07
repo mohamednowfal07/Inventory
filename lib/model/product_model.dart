54b880939b00bae695a57392f7cef2ff007870e8
mohamednowfal07 <nowfal161203@gmail.com>
2025-07-07 20:09:27 +0530

    product list and cart

---
+++ lib/model/product_model.dart
@@ -0,0 +1,16 @@
+// models/product.dart
+class Product {
+  String id;
+  String name;
+  double price;
+  String image;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.image, 
+  });
+
+
+}
